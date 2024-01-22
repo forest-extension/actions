78902882da48e33846ee69e09a75dd52ff66edff
ImMin5 <mino@megazone.com>
2024-01-23 03:28:14 +0900

    build: add setup.py template

--- src/command/template.py
+++ src/command/template.py
@@ -21,17 +21,28 @@ def run_template(**params):
     github_connector = GithubConnector()
 
     service, resource = resource_type.lower().split(".")
+    logging.info(f"[destination] {destination}")
 
     # Create Topics
     topics = [service, resource, workflow_type]
     topics.extend(github_connector.get_topics(destination))
     topics = list(set(topics))
     github_connector.create_topic(destination, topics)
+    logging.info(f"[{destination}] Topics: {topics}")
 
     # Create Dockerfile
     _update_dockerfile(
         github_connector, destination, workflow_type, core_version, service
     )
+
+    # Create setup.py
+    _create_setup_py(github_connector, destination, service, workflow_type)
+
+    # Create VERSION
+    _create_version_file(github_connector, destination)
+
+    # Create empty pip_requirements.txt
+    _create_empty_pkg_file(github_connector, destination)
 
 
 def _update_dockerfile(
@@ -72,6 +83,65 @@ def _update_dockerfile(
         try:
             logging.info(f"[{destination}] Try to create {path} to {destination}")
             github_connector.create_file(destination, path, content)
+        except Exception as e:
+            logging.error(
+                f"[{destination}] Failed to create {path} to {destination} : {e}"
+            )
+
+
+def _create_setup_py(github_connector, destination, service, workflow_type):
+    path = "src/setup.py"
+
+    # Create Dockerfile
+    jinja_env = Environment(
+        autoescape=False,
+        loader=FileSystemLoader(os.path.join(TEMPLATE_DIR, workflow_type)),
+        trim_blocks=True,
+    )
+
+    template = jinja_env.get_template("setup.py.tmpl")
+    content = template.render(
+        destination=destination,
+        repository_name=destination.split("/")[1],
+        plugin_package=f"spaceone-{service.lower()}",
+    )
+
+    if github_connector.get_file_contents(destination, path):
+        logging.info(f"[{destination}] file {path} already exists, pass to create")
+    else:
+        try:
+            logging.info(f"[{destination}] Try to create {path} to {destination}")
+            github_connector.create_file(destination, path, content)
+        except Exception as e:
+            logging.error(
+                f"[{destination}] Failed to create {path} to {destination} : {e}"
+            )
+
+
+def _create_version_file(github_connector, destination):
+    path = "src/VERSION"
+
+    if github_connector.get_file_contents(destination, path):
+        logging.info(f"[{destination}] file {path} already exists, pass to create")
+    else:
+        try:
+            logging.info(f"[{destination}] Try to create {path} to {destination}")
+            github_connector.create_file(destination, path, "")
+        except Exception as e:
+            logging.error(
+                f"[{destination}] Failed to create {path} to {destination} : {e}"
+            )
+
+
+def _create_empty_pkg_file(github_connector, destination):
+    path = "pkg/pip_requirements.txt"
+
+    if github_connector.get_file_contents(destination, path):
+        logging.info(f"[{destination}] file {path} already exists, pass to create")
+    else:
+        try:
+            logging.info(f"[{destination}] Try to create {path} to {destination}")
+            github_connector.create_file(destination, path, "")
         except Exception as e:
             logging.error(
                 f"[{destination}] Failed to create {path} to {destination} : {e}"
